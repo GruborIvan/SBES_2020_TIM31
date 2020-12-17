f8f3a73c54c43dbb841d0eade9228df6af249305
nejvej@gmail.com <nejvej@gmail.com>
2020-12-17 11:51:05 +0100

    Xml handler

---
+++ CryptoProject/XmlHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Common;
+using System.IO;
+
+namespace CryptoProject
+{
+    class XmlHandler
+    {
+        public string EntityToXml(LogEntitet le)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(LogEntitet));
+            using (StringWriter textWriter = new StringWriter()){
+                xmlSerializer.Serialize(textWriter, le);
+                File.WriteAllText("baza.xml", textWriter.ToString());
+                return textWriter.ToString();
+            }
+        }
+    }
+}
