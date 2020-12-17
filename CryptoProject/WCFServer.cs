26853d72c5d35b8f06653ea4cc5425d18888b07d
mtomin367@gmail.com <mtomin367@gmail.com>
2020-12-17 11:38:03 +0100

    Metode kontrakt interfejsa generisane, za klijent i server.

--- CryptoProject/WCFServer.cs
+++ CryptoProject/WCFServer.cs
@@ -9,8 +9,32 @@ namespace CryptoProject
 {
     public class WCFServer : IDatabaseService
     {
+        public string addLogEntity(LogEntitet entitet) {
+            throw new NotImplementedException();
+        }
+
+        public float cityAverageConsumption(string grad) {
+            throw new NotImplementedException();
+        }
+
+        public bool deleteLogEntity(string id) {
+            throw new NotImplementedException();
+        }
+
+        public List<LogEntitet> readEntities(List<Region> regioni) {
+            throw new NotImplementedException();
+        }
+
+        public float regionAverageConsumption(Region reg) {
+            throw new NotImplementedException();
+        }
+
         public void testservermessage(string message) {
             Console.WriteLine(message);
+        }
+
+        public LogEntitet updateConsumption(string id, int month, float consumption) {
+            throw new NotImplementedException();
         }
     }
 }
--- LocalDatabase/WCFClient.cs
+++ LocalDatabase/WCFClient.cs
@@ -17,7 +17,19 @@ namespace LocalDatabase
             factory = this.CreateChannel();
             Console.WriteLine("PROVERA: Namestio sam!");
         }
-        
+
+        public string addLogEntity(LogEntitet entitet) {
+            throw new NotImplementedException();
+        }
+
+        public float cityAverageConsumption(string grad) {
+            throw new NotImplementedException();
+        }
+
+        public bool deleteLogEntity(string id) {
+            throw new NotImplementedException();
+        }
+
         public void Dispose() {
             if (factory != null) {
                 factory = null;
@@ -26,8 +38,20 @@ namespace LocalDatabase
             this.Close();
         }
 
+        public List<LogEntitet> readEntities(List<Region> regioni) {
+            throw new NotImplementedException();
+        }
+
+        public float regionAverageConsumption(Region reg) {
+            throw new NotImplementedException();
+        }
+
         public void testservermessage(string message) {
             factory.testservermessage(message);
+        }
+
+        public LogEntitet updateConsumption(string id, int month, float consumption) {
+            throw new NotImplementedException();
         }
     }
 }
