2c5a21e9622713c8e5a497efe0b545b97426ec75
nejvej@gmail.com <nejvej@gmail.com>
2020-12-23 14:32:27 +0100

    Revert "vracanje na staro"
    This reverts commit 0ffc773fddea3a296ccbf64d341013dcac0a7a80.

--- LocalDatabase/Database.cs
+++ LocalDatabase/Database.cs
@@ -23,11 +23,5 @@ namespace LocalDatabase
             set { repozitorijum = value; }
         }
 
-        public Dictionary<string, LogEntity> EntityList {
-
-            get { return repozitorijum; }
-            set { repozitorijum = value; }
-        }
-
     }
 }
