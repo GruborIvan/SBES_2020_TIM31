5dacc9bec79445b42bd3b3c4f9be5e972a50907f
nejvej@gmail.com <nejvej@gmail.com>
2020-12-24 23:07:37 +0100

    Dual conn

--- LocalDBase/Program.cs
+++ LocalDBase/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Client;
+using Common;
+using CryptoProject;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,11 +14,22 @@ namespace LocalDBase
     {
         static void Main(string[] args)
         {
-            NetTcpBinding binding = new NetTcpBinding();
-            string address = "net.tcp://localhost:9999/wcfserver";
+            NetTcpBinding bindingClient = new NetTcpBinding();
+            string addressSelf = "net.tcp://localhost:8888/wcfserver";
+
+            ServiceHost host = new ServiceHost(typeof(WCFServer));
+            host.AddServiceEndpoint(typeof(IDatabaseService), bindingClient, addressSelf);
+
+            host.Open();
+
+            Console.WriteLine("WCFService is opened. Press <enter> to finish...");
+            Console.ReadLine();
+
+            NetTcpBinding bindingServer = new NetTcpBinding();
+            string addressServer = "net.tcp://localhost:9999/wcfserver";
 
             CallbackClient callbackclient = new CallbackClient();
-            WCFClient proxy = new WCFClient(callbackclient, binding, new EndpointAddress(new Uri(address)));
+            WCFClient proxy = new WCFClient(callbackclient, bindingServer, new EndpointAddress(new Uri(addressServer)));
             callbackclient.Proxy = proxy;
         }
     }
