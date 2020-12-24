fa363dede2168afd511a713f41871d119f0542ce
nejvej@gmail.com <nejvej@gmail.com>
2020-12-24 22:53:54 +0100

    com za pull

--- LocalDB/Program.cs
+++ LocalDB/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using LocalDatabase;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,13 @@ namespace LocalDB
     {
         static void Main(string[] args)
         {
+            NetTcpBinding binding = new NetTcpBinding();
+            string address = "net.tcp://localhost:9999/wcfserver";
+
+            CallbackClient callbackclient = new CallbackClient();
+            WCFClient proxy = new WCFClient(callbackclient, binding, new EndpointAddress(new Uri(address)));
+            callbackclient.Proxy = proxy;
+            Database database = new Database();
         }
     }
 }
