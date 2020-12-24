020706b956adf851b3a1cfde88ef41802a28d893
nejvej@gmail.com <nejvej@gmail.com>
2020-12-24 23:00:44 +0100

    LocalDb

---
+++ LocalDBase/Program.cs
@@ -0,0 +1,23 @@
+﻿using LocalDatabase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalDBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetTcpBinding binding = new NetTcpBinding();
+            string address = "net.tcp://localhost:9999/wcfserver";
+
+            CallbackClient callbackclient = new CallbackClient();
+            WCFClient proxy = new WCFClient(callbackclient, binding, new EndpointAddress(new Uri(address)));
+            callbackclient.Proxy = proxy;
+        }
+    }
+}
