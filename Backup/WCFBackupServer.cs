df15347f284ff5c6788e48e22f62fc92f52fc508
nejvej@gmail.com <nejvej@gmail.com>
2021-01-01 18:02:03 +0100

    ok radi

---
+++ Backup/WCFBackupServer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace Backup
+{
+    public class WCFBackupServer : IBackupServer
+    {
+        public bool sendChanges(List<Tuple<OperationCode, LogEntity>> promena)
+        {
+            Console.WriteLine("radi");
+            return true;
+        }
+    }
+}
